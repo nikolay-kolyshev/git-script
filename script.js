996d228e587fe48db61c1cfd74ce5f60a4517c6b
Колышев Николай Игоревич <NIKolyshev@sberbank.ru>
2021-12-19 20:44:28 +0300

    script add

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const simpleGit = require('simple-git');
 const git = simpleGit();
 
 const init = async () => {
-    const info = await git.log()
+    const info = await git.log(['origin/master..origin/test-branch-1'])
     console.log(info)
 }
 
